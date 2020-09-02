bd367712e441fd67369bd6485b81d4a15c1f5903
tmp64 <tmp32x2@gmail.com>
2020-09-02 20:32:31 +0700

    tier2: Attempt to fix steamclient.so loading

--- tier2/steam_client_017.cpp
+++ tier2/steam_client_017.cpp
@@ -1,6 +1,11 @@
 #define VERSION_SAFE_STEAM_API_INTERFACES
+#include <tier0/dbg.h>
 #include <tier1/interface.h>
 #include <steam/steam_api.h>
+
+#ifndef PLATFORM_WINDOWS
+#include <dlfcn.h>
+#endif
 
 static ISteamClient017 *s_SteamClient017 = nullptr;
 
@@ -11,10 +16,23 @@ ISteamClient017 *SteamClient017()
 
 void InitSteamClient017()
 {
+	if (s_SteamClient017)
+		return;
+
     // steamclient.dll should be loaded by steam_api
+#ifdef PLATFORM_WINDOWS
     CSysModule *steamClientModule = Sys_LoadModule("steamclient" DLL_EXT_STRING);
+#else
+    CSysModule *steamClientModule = reinterpret_cast<CSysModule *>(dlopen("steamclient" DLL_EXT_STRING, RTLD_NOW));
+#endif
+	
     if (!steamClientModule)
-        return;
+	{
+#ifndef PLATFORM_WINDOWS
+	    Warning("Failed to load steamclient" DLL_EXT_STRING ": %s\n", dlerror());
+#endif
+		return;
+	}
 
     CreateInterfaceFn factory = Sys_GetFactory(steamClientModule);
     if (!factory)
